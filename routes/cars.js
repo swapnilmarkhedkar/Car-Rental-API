8f740aec6e3a288a8474cfee5f6be2830fd64a68
Swapnil Markhedkar <swapnilmarkhedkar@gmail.com>
2020-05-08 15:37:27 +0530

    Added new GET end point that uses previously created function

--- routes/cars.js
+++ routes/cars.js
@@ -69,8 +69,57 @@ router.get('/date/:pickupDate/:dropDate', (req,res)=>{
     // Use Car.populate('bookings'), however this would require a bookings array associated with each car
 });
 
-function isCurrCarBooked(car){
-    var query = utils.returnCurrentDateQuery(car.id);
+// GET cars by date and check if they are booked
+router.get('/newdate/:pickupDate/:dropDate', (req,res)=>{
+    var fromDate = req.params.pickupDate;
+    var toDate = req.params.dropDate;
+    // @TODO: Validate dates
+
+    carList = [];
+    Car.find(req.query).then((cars)=>{
+        // req.query is used to add additional query parameter like ?seatingCapacity=5&rent=1000
+
+        // Look through all cars and check if booked for that period
+        for(var i=0; i<cars.length; i++){
+            let car=cars[i];
+            // Check if that car is available in specified date range
+          
+            var query = utils.returnDateQuery(car.id,toDate,fromDate);
+            // Promise adds to list if condition is satisfied
+            carList.push(new Promise (function(resolve, reject){
+                isCurrCarBooked(query).then(()=>{
+                    // Promise resolved which means currently not booked
+                    resolve(car);             
+                }).catch(()=>{
+                    // Promise rejected and thus booked
+                    resolve();
+                });
+            }));
+        }
+
+        // Waits till all requests are satisfied
+        Promise.all(carList).then(function(results){
+            finalList= [];
+
+            // Add to list non-null elements
+            for(var i=0; i<results.length; i++){
+                if(results[i])
+                    finalList.push(results[i]);
+            }
+            res.send(finalList);
+
+        }).catch((e)=>{
+            res.status(400).send(e);
+        });
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+
+    // Alternative
+    // Use Car.populate('bookings'), however this would require a bookings array associated with each car
+});
+
+function isCurrCarBooked(query){
     return new Promise((resolve, reject)=>{
         Booking.find(query, function (err, bookings){
             if(err) reject(err); // Handle error
@@ -100,7 +149,8 @@ router.get('/:id', (req,res)=>{
             return res.status(404).send();
         }
         
-        isCurrCarBooked(car).then(()=>{
+        var query = utils.returnCurrentDateQuery(car.id);
+        isCurrCarBooked(query).then(()=>{
             // Promise resolved which means currently not booked
             res.send({
                 car,
